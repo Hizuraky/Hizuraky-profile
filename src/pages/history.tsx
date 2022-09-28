82da65ff9091ab6c25ffac700ef3bf1dfe744b90
Hizuraky <hizuraky@gmail.com>
2022-09-29 01:06:09 +0900

    add eslint/prettier/tailwind/pages /gatsbyPlugins

---
+++ src/pages/history.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function(){
+  return(
+    <div>
+      History
+    </div>
+  )
+}
+
+// export default HistoryPage
---
+++ src/pages/skill.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function(){
+  return(
+    <div>
+      Skill
+    </div>
+  )
+}
+
+// export default HistoryPage
